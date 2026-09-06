3d2855c04874d3e08d802f386388459f5093b02f
theotg <kennythesman@gmail.com>
2026-09-06 10:51:53 +0700

    fixed regex for tr trade update

--- app/shared/resources/RegExes/RegExes.js
+++ app/shared/resources/RegExes/RegExes.js
@@ -1,1 +1,1 @@
-export const poeTradeUrl = /\/([a-zA-Z0-9]+)\/search\/(?:poe2\/)?([a-zA-Z0-9%]+)\/([a-zA-Z0-9]+)/;
+export const poeTradeUrl = /\/([a-zA-Z0-9]+)\/search\/(?:poe2\/)?([a-zA-Z0-9%]+)\/([a-zA-Z0-9_-]+)/;
